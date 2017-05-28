fc6d4d5e829a5b85e3169ebddb017ffe802ad1c7
BeejLuig <bjcantlupe@gmail.com>
2017-05-28 17:30:56 -0400

    Add namsepacing to controller

--- app/controllers/api/v1/runners_controller.rb
+++ app/controllers/api/v1/runners_controller.rb
@@ -1,11 +1,14 @@
-class RunnersController < ApplicationController
+class Api::V1::RunnersController < ApplicationController
   def index
+    @runners = Runner.all
+    @total_distance = Runner.total_distance
 
+    render 'runners/index.json.jbuilder'
   end
 
   def new
 
   end
 
-  
+
 end
