04be5b5ba3dad8637359868075451340cf4864ec
BeejLuig <bjcantlupe@gmail.com>
2017-05-28 17:30:22 -0400

    Add default value to runner name

--- db/migrate/20170528160312_create_runners.rb
+++ db/migrate/20170528160312_create_runners.rb
@@ -1,7 +1,7 @@
 class CreateRunners < ActiveRecord::Migration[5.0]
   def change
     create_table :runners do |t|
-      t.string :name
+      t.string :name, default: "Anonymous"
       t.integer :distance
 
       t.timestamps
